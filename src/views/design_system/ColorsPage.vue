51f65a11fdb915edc2bb039de7c3a6a21b984ab8
Dipanjan <dipanjan131@gmail.com>
2023-03-08 16:52:49 +0530

    Added Color Modifications

--- src/views/design_system/ColorsPage.vue
+++ src/views/design_system/ColorsPage.vue
@@ -10,28 +10,32 @@
 					v-model="colors.dark",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors['light-faded']"
+					:overlay-color="colors['light-faded']",
+					@update="handleChange"
 				)
 				ColorField(
 					label="Dark - Faded",
 					v-model="colors['dark-faded']",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors.light"
+					:overlay-color="colors.light",
+					@update="handleChange"
 				)
 				ColorField(
 					label="Light",
 					v-model="colors.light",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors['dark-faded']"
+					:overlay-color="colors['dark-faded']",
+					@update="handleChange"
 				)
 				ColorField(
 					label="Light - Faded",
 					v-model="colors['light-faded']",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors.dark"
+					:overlay-color="colors.dark",
+					@update="handleChange"
 				)
 
 		.section
@@ -42,28 +46,32 @@
 					v-model="colors.primary",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors['primary-contrast']"
+					:overlay-color="colors['primary-contrast']",
+					@update="handleChange"
 				)
 				ColorField(
 					label="Primary - Contrast",
 					v-model="colors['primary-contrast']",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors['primary']"
+					:overlay-color="colors['primary']",
+					@update="handleChange"
 				)
 				ColorField(
 					label="Primary Variant",
 					v-model="colors['primary-variant']",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors['primary-variant-contrast']"
+					:overlay-color="colors['primary-variant-contrast']",
+					@update="handleChange"
 				)
 				ColorField(
 					label="Primary Variant - Contrast",
 					v-model="colors['primary-variant-contrast']",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors['primary-variant']"
+					:overlay-color="colors['primary-variant']",
+					@update="handleChange"
 				)
 
 		.section
@@ -74,28 +82,32 @@
 					v-model="colors.secondary",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors['secondary-contrast']"
+					:overlay-color="colors['secondary-contrast']",
+					@update="handleChange"
 				)
 				ColorField(
 					label="Secondary - Contrast",
 					v-model="colors['secondary-contrast']",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors['secondary']"
+					:overlay-color="colors['secondary']",
+					@update="handleChange"
 				)
 				ColorField(
 					label="Secondary Variant",
 					v-model="colors['secondary-variant']",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors['secondary-variant-contrast']"
+					:overlay-color="colors['secondary-variant-contrast']",
+					@update="handleChange"
 				)
 				ColorField(
 					label="Secondary Variant - Contrast",
 					v-model="colors['secondary-variant-contrast']",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors['secondary-variant']"
+					:overlay-color="colors['secondary-variant']",
+					@update="handleChange"
 				)
 
 		.section
@@ -106,28 +118,32 @@
 					v-model="colors.hyperlink",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors.dark"
+					:overlay-color="colors.dark",
+					@update="handleChange"
 				)
 				ColorField(
 					label="Offline",
 					v-model="colors.offline",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors.dark"
+					:overlay-color="colors.dark",
+					@update="handleChange"
 				)
 				ColorField(
 					label="Divider",
 					v-model="colors.divider",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors.dark"
+					:overlay-color="colors.dark",
+					@update="handleChange"
 				)
 				ColorField(
 					label="Disabled",
 					v-model="colors.disabled",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors.dark"
+					:overlay-color="colors.dark",
+					@update="handleChange"
 				)
 
 		.section
@@ -138,56 +154,64 @@
 					v-model="colors.error",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors.dark"
+					:overlay-color="colors.dark",
+					@update="handleChange"
 				)
 				ColorField(
 					label="Success",
 					v-model="colors.success",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors.dark"
+					:overlay-color="colors.dark",
+					@update="handleChange"
 				)
 				ColorField(
 					label="Warning",
 					v-model="colors.warning",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors.dark"
+					:overlay-color="colors.dark",
+					@update="handleChange"
 				)
 				ColorField(
 					label="Information",
 					v-model="colors.information",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors.dark"
+					:overlay-color="colors.dark",
+					@update="handleChange"
 				)
 				ColorField(
 					label="Error Variant",
 					v-model="colors['error-variant']",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors.dark"
+					:overlay-color="colors.dark",
+					@update="handleChange"
 				)
 				ColorField(
 					label="Success Variant",
 					v-model="colors['success-variant']",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors.dark"
+					:overlay-color="colors.dark",
+					@update="handleChange"
 				)
 				ColorField(
 					label="Warning Variant",
 					v-model="colors['warning-variant']",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors.dark"
+					:overlay-color="colors.dark",
+					@update="handleChange"
 				)
 				ColorField(
 					label="Information Variant",
 					v-model="colors['information-variant']",
 					:outline="true",
 					:overlay="true",
-					:overlay-color="colors.dark"
+					:overlay-color="colors.dark",
+					@update="handleChange"
 				)
 
 		MarkdownParser(:content="$t('pages.design_system.colors.content')")
@@ -234,6 +258,11 @@ export default defineComponent({
 				"information-variant": "#a6ccf1",
 			},
 		};
+	},
+	methods: {
+		handleChange(): void {
+			this.$ecosphere.theming.setColors(this.colors);
+		},
 	},
 });
 </script>
